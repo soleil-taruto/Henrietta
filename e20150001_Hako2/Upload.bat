49b1fe53c95858547b0d7ba4f8b7774f6ee97492
soleil-taruto <soleil.taruto@gmail.com>
2021-03-12 18:48:32 +0900

    Backup 20210312184831

--- e20150001_Hako2/Upload.bat
+++ e20150001_Hako2/Upload.bat
@@ -8,5 +8,5 @@ C:\Factory\Petra\EditConfig.exe Hako2\Hako2\Define.h "// [LOG_ENABLED_SWITCH]" /
 	CALL Release.bat /B
 rem	CALL Release.bat /V 001
 
-C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\Henrietta\Hako2
+C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\Henrietta\d20150001_Hako2
 C:\Factory\Petra\RunOnBatch.exe C:\be\Web Upload.bat
